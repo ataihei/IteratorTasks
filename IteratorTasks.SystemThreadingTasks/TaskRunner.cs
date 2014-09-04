ac22a05c7b27773951eca7bf7f18d4774c11272d
niwanaga <niwanaga@aiming-inc.com>
2014-09-04 12:01:35 +0900

    タブ→スペース4つ(再)、interopがらみ

--- IteratorTasks.SystemThreadingTasks/TaskRunner.cs
+++ IteratorTasks.SystemThreadingTasks/TaskRunner.cs
@@ -5,66 +5,66 @@ using TT = System.Threading.Tasks;
 
 namespace IteratorTasks
 {
-	/// <summary>
-	/// スレッドプールで <see cref="IteratorTasks.TaskScheduler.Update"/> を回すクラス。
-	/// 互換性用。
-	/// 1 Runner 1 Scheduler なんだけど、1 Runner が複数持つように変えたいけど、既存のものはそっとしておきたいので別クラスを作った → <see cref="MultiTaskRunner"/>。
-	/// </summary>
-	public class TaskRunner
-	{
-		private TT.Task _task;
-		private volatile bool _isAlive;
-		private ST.CancellationTokenSource _cts = new ST.CancellationTokenSource();
+    /// <summary>
+    /// スレッドプールで <see cref="IteratorTasks.TaskScheduler.Update"/> を回すクラス。
+    /// 互換性用。
+    /// 1 Runner 1 Scheduler なんだけど、1 Runner が複数持つように変えたいけど、既存のものはそっとしておきたいので別クラスを作った → <see cref="MultiTaskRunner"/>。
+    /// </summary>
+    public class TaskRunner
+    {
+        private TT.Task _task;
+        private volatile bool _isAlive;
+        private ST.CancellationTokenSource _cts = new ST.CancellationTokenSource();
 
-		public IT.TaskScheduler Scheduler { get; private set; }
-		public ST.CancellationToken Token { get { return _cts.Token; } }
+        public IT.TaskScheduler Scheduler { get; private set; }
+        public ST.CancellationToken Token { get { return _cts.Token; } }
 
-		public bool HasError { get; private set; }
+        public bool HasError { get; private set; }
 
-		public TaskRunner() : this(new IT.TaskScheduler()) { }
+        public TaskRunner() : this(new IT.TaskScheduler()) { }
 
-		public TaskRunner(IT.TaskScheduler scheduler)
-		{
-			Scheduler = scheduler;
-			_task = UpdateLoop(scheduler);
-		}
+        public TaskRunner(IT.TaskScheduler scheduler)
+        {
+            Scheduler = scheduler;
+            _task = UpdateLoop(scheduler);
+        }
 
-		private async TT.Task UpdateLoop(IT.TaskScheduler scheduler)
-		{
-			_isAlive = true;
-			while (_isAlive)
-			{
-				try
-				{
-					scheduler.Update();
+        private async TT.Task UpdateLoop(IT.TaskScheduler scheduler)
+        {
+            _isAlive = true;
+            while (_isAlive)
+            {
+                try
+                {
+                    scheduler.Update();
 
-					var delayMilliseconds = scheduler.IsActive ? 5 : 50;
-					await TT.Task.Delay(delayMilliseconds).ConfigureAwait(false);
-					// ↑Delay なし、専用スレッドで回りっぱなしとかがいいかもしれないし。
-				}
-				catch (Exception ex)
-				{
-					HasError = true;
-					OnError(ex);
-				}
-			}
-		}
+                    var delayMilliseconds = scheduler.IsActive ? 5 : 50;
+                    await TT.Task.Delay(delayMilliseconds).ConfigureAwait(false);
+                    // ↑Delay なし、専用スレッドで回りっぱなしとかがいいかもしれないし。
+                }
+                catch (Exception ex)
+                {
+                    HasError = true;
+                    OnError(ex);
+                }
+            }
+        }
 
-		public TT.Task Stop()
-		{
-			_cts.Cancel();
-			_isAlive = false;
-			return _task;
-		}
+        public TT.Task Stop()
+        {
+            _cts.Cancel();
+            _isAlive = false;
+            return _task;
+        }
 
-		public TT.Task Task { get { return _task; } }
+        public TT.Task Task { get { return _task; } }
 
-		public event EventHandler<Exception> Error;
+        public event EventHandler<Exception> Error;
 
-		private void OnError(Exception ex)
-		{
-			var d = Error;
-			if (d != null) d(this, ex);
-		}
-	}
+        private void OnError(Exception ex)
+        {
+            var d = Error;
+            if (d != null) d(this, ex);
+        }
+    }
 }
--- IteratorTasks.SystemThreadingTasks/TaskUtil/TaskPool.cs
+++ IteratorTasks.SystemThreadingTasks/TaskUtil/TaskPool.cs
@@ -4,19 +4,19 @@ using System.Threading.Tasks;
 
 namespace TaskUtil
 {
-	// 同名のクラスが IteratorTasks の方にもあるけど、それの System.Threading.Tasks 版。
-	// この名前空間以下でいいのかは微妙（もっと汎用）だけど、移すかどうかは必要になってから考える。
-	public class TaskPool
-	{
-		private readonly List<Task> _list = new List<Task>();
+    // 同名のクラスが IteratorTasks の方にもあるけど、それの System.Threading.Tasks 版。
+    // この名前空間以下でいいのかは微妙（もっと汎用）だけど、移すかどうかは必要になってから考える。
+    public class TaskPool
+    {
+        private readonly List<Task> _list = new List<Task>();
 
-		public void Register(Task t) { _list.Add(t); }
+        public void Register(Task t) { _list.Add(t); }
 
-		public Task Await()
-		{
-			var copy = _list.ToArray();
-			_list.Clear();
-			return Task.WhenAll(copy);
-		}
-	}
+        public Task Await()
+        {
+            var copy = _list.ToArray();
+            _list.Clear();
+            return Task.WhenAll(copy);
+        }
+    }
 }
