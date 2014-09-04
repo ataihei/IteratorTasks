3bd847385b5bcb1003ecb5e8bad7acc7c4c2ff4d
niwanaga <niwanaga@aiming-inc.com>
2014-09-04 12:00:43 +0900

    IteratorTasksと.NET標準Taskのinterop系の処理をIteratorTasks.SystemThreadingTasksプロジェクトとして追加。

---
+++ IteratorTasks.SystemThreadingTasks/MultiTaskRunner.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using IT = IteratorTasks;
+using ST = System.Threading;
+using TT = System.Threading.Tasks;
+
+namespace IteratorTasks
+{
+	/// <summary>
+	/// 複数のスケジューラーを、同時に1個ずつしか動かさない保証付で Update を掛けるクラス。
+	/// あくまで、このランナーに登録したスケジューラーは1度に1個しか動かないというだけで、実行スレッドはころころ変わるので注意(Sleep じゃなくて await Delay してるので)。
+	/// もちろん、別ランナーに登録したスケジューラーは同時に複数動く。
+	/// </summary>
+	public class MultiTaskRunner
+	{
+		private TT.Task _task;
+		private volatile bool _isAlive;
+		private ST.CancellationTokenSource _cts = new ST.CancellationTokenSource();
+
+		public IEnumerable<IT.TaskScheduler> Schedulers { get { return _schedulers; } }
+		private ImmutableList<IT.TaskScheduler> _schedulers = ImmutableList<IT.TaskScheduler>.Empty;
+		public ST.CancellationToken Token { get { return _cts.Token; } }
+
+		public bool HasError { get; private set; }
+
+		public MultiTaskRunner()
+		{
+			_task = UpdateLoop();
+		}
+
+		private async TT.Task UpdateLoop()
+		{
+			_isAlive = true;
+			while (_isAlive)
+			{
+				var any = false;
+
+				foreach (var s in _schedulers)
+				{
+					try
+					{
+						s.Update();
+					}
+					catch (Exception ex)
+					{
+						HasError = true;
+						OnError(ex, s);
+					}
+					any |= s.IsActive;
+				}
+
+				var delayMilliseconds = any ? 1 : 50;
+				await TT.Task.Delay(delayMilliseconds).ConfigureAwait(false);
+			}
+		}
+
+		public TT.Task Stop()
+		{
+			_cts.Cancel();
+			_isAlive = false;
+			return _task;
+		}
+
+		public void Add(IT.TaskScheduler s)
+		{
+			_schedulers = _schedulers.Add(s);
+		}
+
+		public void Remove(IT.TaskScheduler s)
+		{
+			_schedulers = _schedulers.Remove(s);
+		}
+
+		public TT.Task Task { get { return _task; } }
+
+		public event EventHandler<ErrorHandlerAgs> Error;
+
+		public class ErrorHandlerAgs
+		{
+			public Exception Error { get; private set; }
+			public IT.TaskScheduler Scheduler { get; private set; }
+
+			public ErrorHandlerAgs(Exception error, IT.TaskScheduler scheduler)
+			{
+				Error = error;
+				Scheduler = scheduler;
+			}
+		}
+
+		private void OnError(Exception ex, IT.TaskScheduler scheduler)
+		{
+			var d = Error;
+			if (d != null) d(this, new ErrorHandlerAgs(ex, scheduler));
+		}
+	}
+}
---
+++ IteratorTasks.SystemThreadingTasks/TaskExtensions.cs
@@ -0,0 +1,115 @@
+﻿using T = System.Threading;
+using TT = System.Threading.Tasks;
+using System.Linq;
+
+namespace IteratorTasks
+{
+    /// <summary>
+    /// IteratorTasks と .NET 標準 Task の interop 用拡張メソッド群。
+    /// </summary>
+    public static class TaskExtensions
+    {
+        public static TaskRunner BeginUpdate(this TaskScheduler scheduler)
+        {
+#if false
+            // 複数個所で同じスケジューラーに対してこいつを呼ばれるとやばいので、変な挙動した場合、ここの ifdef true にして呼び出し元探してみる。
+            var s = new System.Diagnostics.StackTrace();
+            var sid = scheduler.Id;
+            System.Diagnostics.Debug.WriteLine("scheduler " + sid + "runs on " + s.ToString());
+#endif
+
+            return new TaskRunner(scheduler);
+        }
+
+        public static void DebugWriteOnUnhandledException(this TaskScheduler scheduler)
+        {
+            scheduler.UnhandledException += scheduler_UnhandledException;
+        }
+
+        private static void scheduler_UnhandledException(Task obj)
+        {
+            var e = obj.Error;
+            var a = e as AggregateException;
+
+            if (a == null || !a.Exceptions.Any(x => x is TaskCanceledException))
+            {
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+            }
+        }
+
+        public static Task ToIterator(this TT.Task t, TaskScheduler scheduler)
+        {
+            var tcs = new TaskCompletionSource<object>(scheduler);
+            t.ContinueWith(x =>
+                {
+                    if (x.IsCompleted)
+                        tcs.SetResult(null);
+                    else if (x.IsFaulted)
+                        tcs.SetException(x.Exception);
+                    else
+                        tcs.SetCanceled();
+                });
+            return tcs.Task;
+        }
+
+        public static Task<T> ToIterator<T>(this TT.Task<T> t, TaskScheduler scheduler)
+        {
+            var tcs = new TaskCompletionSource<T>(scheduler);
+            t.ContinueWith(x =>
+                {
+                    if (x.IsCanceled)
+                        tcs.SetCanceled();
+                    else if (x.IsFaulted)
+                        tcs.SetException(x.Exception);
+                    else
+                        tcs.SetResult(x.Result);
+                });
+            return tcs.Task;
+        }
+
+        public static TT.Task ToTpl(this Task t)
+        {
+            var tcs = new TT.TaskCompletionSource<bool>();
+            t.ContinueWith(x =>
+                {
+                    if (x.IsCanceled)
+                        tcs.SetCanceled();
+                    else if (x.IsFaulted)
+                        tcs.SetException(x.Error);
+                    else
+                        tcs.SetResult(false);
+                });
+            return tcs.Task;
+        }
+
+        public static TT.Task<T> ToTpl<T>(this Task<T> t)
+        {
+            var tcs = new TT.TaskCompletionSource<T>();
+            t.ContinueWith(x =>
+                {
+                    if (x.IsCanceled)
+                        tcs.SetCanceled();
+                    else if (x.IsFaulted)
+                        tcs.SetException(x.Error);
+                    else
+                        tcs.SetResult(t.Result);
+                });
+            return tcs.Task;
+        }
+
+        public static CancellationToken ToIterator(this T.CancellationToken ct)
+        {
+            var cts = new CancellationTokenSource();
+            ct.Register(() => cts.Cancel());
+            return cts.Token;
+        }
+
+        public static T.CancellationToken ToTpl(this CancellationToken ct)
+        {
+            var cts = new T.CancellationTokenSource();
+            ct.Register(() => cts.Cancel());
+            return cts.Token;
+        }
+    }
+
+}
