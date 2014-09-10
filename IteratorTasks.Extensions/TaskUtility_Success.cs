97a9f0eeedba71fe055d1d1e6b968021fffb97d2
oc-ratih <ysogabe@orange-cube.jp>
2014-09-10 15:46:21 +0900

    ExtensionsにOnSuccess追加

---
+++ IteratorTasks.Extensions/TaskUtility_Success.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace IteratorTasks
+{
+    public static partial class TaskUtility
+    {
+
+        public static Task OnSuccess(this Task t, Action succesHandler)
+        {
+            return t.ContinueWith(x =>
+            {
+                succesHandler();
+            }, true);
+        }
+
+        public static Task OnSuccess<T>(this Task<T> t, Action<T> succesHandler)
+        {
+            return t.ContinueWith(x =>
+            {
+                succesHandler(t.Result);
+            }, true);
+        }
+
+        public static Task<T> OnSuccess<T>(this Task t, Func<T> succesHandler)
+        {
+            return t.ContinueWith<T>(x =>
+            {
+                return succesHandler();
+            }, true);
+        }
+
+        public static Task<U> OnSuccess<T, U>(this Task<T> t, Func<T, U> succesHandler)
+        {
+            return t.ContinueWith<U>(x =>
+            {
+                return succesHandler(t.Result);
+            }, true);
+        }
+
+        public static Task OnSuccessWithTask(this Task t, Func<Task> succesHandler)
+        {
+            return t.ContinueWithTask(x =>
+            {
+                return succesHandler();
+            }, true);
+        }
+
+        public static Task OnSuccessWithTask<T>(this Task<T> t, Func<T, Task> succesHandler)
+        {
+            return t.ContinueWithTask(x =>
+            {
+                return succesHandler(t.Result);
+            }, true);
+        }
+
+        public static Task<T> OnSuccessWithTask<T>(this Task t, Func<Task<T>> succesHandler)
+        {
+            return t.ContinueWithTask<T>(x =>
+            {
+                return succesHandler();
+            }, true);
+        }
+
+        public static Task<U> OnSuccessWithTask<T, U>(this Task<T> t, Func<T, Task<U>> succesHandler)
+        {
+            return t.ContinueWithTask<U>(x =>
+            {
+                return succesHandler(t.Result);
+            }, true);
+        }
+    }
+}
