46bad4d18603c5b34e8fc792d952d8f71b200a5c
oc-ratih <ysogabe@orange-cube.jp>
2014-09-11 11:42:58 +0900

    IteratorTask,テストケース追加
    WhenAny, await task, タイミングテストの追加

---
+++ TestIteratorTasks/TaskWhenTest.cs
@@ -0,0 +1,72 @@
+﻿using IteratorTasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Linq;
+
+namespace TestIteratorTasks
+{
+	[TestClass]
+	public class TaskWhenTest
+	{
+		[TestMethod]
+		public void WhenAllでタスクの並行動作できる()
+		{
+			var t1 = Task.Run(() => Coroutines.NFrame(3));
+			var t2 = Task.Run(() => Coroutines.NFrame(5));
+			var t3 = Task.Run(() => Coroutines.NFrame(7));
+
+			var task = Task.WhenAll(t1, t2, t3)
+				.OnComplete(t =>
+				{
+					Assert.IsTrue(t1.IsCompleted);
+					Assert.IsTrue(t2.IsCompleted);
+					Assert.IsTrue(t3.IsCompleted);
+				});
+
+			var scheduler = Task.DefaultScheduler;
+			scheduler.Update(20);
+
+			Assert.IsTrue(task.IsCompleted);
+			Assert.IsNull(task.Error);
+		}
+
+		[TestMethod]
+		public void WhenAnyで何か1つのタスクが終わるのを待てる()
+		{
+			var t = Task.Run(WhenAnyで何か1つのタスクが終わるのを待てるIterator);
+
+			var scheduler = Task.DefaultScheduler;
+			scheduler.Update(15);
+		}
+
+		private IEnumerator WhenAnyで何か1つのタスクが終わるのを待てるIterator()
+		{
+			var delays = new[] { 3, 5, 7, 9, 11, 13 };
+
+			var tasks = delays.Select(d => Task.Run(() => Coroutines.NFrame(d))).ToArray();
+
+			while (true)
+			{
+				var task = Task.WhenAny(tasks);
+
+				yield return task;
+
+				var first = task.Result;
+
+				// delays が昇順で並んでいるので、前のタスクから終わるはず
+				Assert.AreEqual(first, tasks[0]);
+
+				// delays に同じ値を入れていないので、同時に1個ずつしか終わらないはず
+				foreach (var t in tasks.Skip(1))
+				{
+					Assert.IsFalse(t.IsCompleted);
+				}
+
+				// 終わったやつを除外。全部終わったら終了
+				tasks = tasks.Where(t => t != first).ToArray();
+				if (tasks.Length == 0)
+					break;
+			}
+		}
+	}
+}
---
+++ TestIteratorTasks/TaskYieldAwaitTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IteratorTasks;
+using System.Collections;
+
+namespace TestIteratorTasks
+{
+	[TestClass]
+	public class TaskYieldAwaitTest
+	{
+		[TestMethod]
+		public void yield_returnでawait的なことができる()
+		{
+			const int MaxDelay = 1000;
+			var scheduler = new TaskSchedulerHelper(Task.DefaultScheduler);
+
+			int i = 0;
+
+			Task.Run(yield_returnでawait的なことができるIterator(MaxDelay, scheduler));
+
+			for (i = 0; i < MaxDelay; i++)
+			{
+				scheduler.Update();
+			}
+
+		}
+
+		private IEnumerator yield_returnでawait的なことができるIterator(int maxDelay, TaskSchedulerHelper s)
+		{
+			var random = new Random();
+			int n = random.Next(19, maxDelay);
+
+			var tasks = Enumerable.Range(0, 5).Select(_ => Task.Run(Coroutines.NFrame(n))).ToArray();
+
+			foreach (var t in tasks)
+			{
+				if (t.IsCompleted)
+				{
+					// 完了済みのタスクを yield return すると、1フレーム後にすぐ戻ってくるはず
+					var countBefore = s.FrameCount;
+					yield return t;
+					var countAfter = s.FrameCount;
+
+					Assert.AreEqual(countBefore + 1, countAfter);
+				}
+				else
+				{
+					yield return t;
+					// yield return するとタスク完了待てる
+					Assert.IsTrue(t.IsCompleted);
+				}
+			}
+
+			foreach (var t in tasks)
+			{
+				Assert.IsTrue(t.IsCompleted);
+			}
+		}
+	}
+}
