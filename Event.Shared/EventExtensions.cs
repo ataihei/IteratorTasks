9323055e12a2b269cf36b34ef9fbe888ea415215
niwanaga <iwanaga@orange-cube.jp>
2014-09-22 12:09:41 +0900

    IEventをTaskNavigationからこちらに移動
    専用プロジェクトを1つ。
    名前空間はSystemにしてしまった。手抜きは手抜きなんだけども。よい名前もなく(Commonとかしか思いつかない)。

---
+++ Event.Shared/EventExtensions.cs
@@ -0,0 +1,87 @@
+﻿#if UseIteratorTasks
+using IteratorTasks;
+#else
+using System.Threading;
+using System.Threading.Tasks;
+#endif
+
+namespace System
+{
+    public static partial class EventExtensions
+    {
+        /// <summary>
+        /// イベントを1回だけ受け取る。
+        /// </summary>
+        public static Task<TArg> FirstAsync<TArg>(this IEvent<TArg> e, CancellationToken ct)
+        {
+            return FirstAsync(e, _ => true, ct);
+        }
+
+        /// <summary>
+        /// イベントを1回だけ受け取る。
+        /// predicate を満たすまでは何回でもイベントを受け取る。
+        /// </summary>
+        /// <typeparam name="TArg">イベント引数の型。</typeparam>
+        /// <param name="e">イベント発生元。</param>
+        /// <param name="predicate">受け取り条件。</param>
+        /// <param name="ct">キャンセル用。</param>
+        /// <returns>イベントが1回起きるまで待つタスク。</returns>
+        public static Task<TArg> FirstAsync<TArg>(this IEvent<TArg> e, Func<TArg, bool> predicate, CancellationToken ct)
+        {
+            var tcs = new TaskCompletionSource<TArg>();
+
+            Handler<TArg> a = null;
+            a = (sender, args) =>
+            {
+                if (predicate(args))
+                {
+                    e.Remove(a);
+                    tcs.TrySetResult(args);
+                }
+            };
+
+            e.Add(a);
+
+            ct.Register(() =>
+            {
+                e.Remove(a);
+                tcs.TrySetCanceled();
+            });
+
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// イベントを1回だけ受け取る。
+        /// predicate を満たすまでは何回でもイベントを受け取る。
+        /// predicate が非同期処理なバージョン。
+        /// </summary>
+        public static Task<TArg> FirstAsync<TArg>(this IEvent<TArg> e, Func<TArg, Task<bool>> predicate, CancellationToken ct)
+        {
+            var tcs = new TaskCompletionSource<TArg>();
+
+            Handler<TArg> a = null;
+            a = (sender, args) =>
+            {
+                predicate(args).ContinueWith(t =>
+                {
+                    if (t.Result)
+                    {
+                        e.Remove(a);
+                        tcs.TrySetResult(args);
+                    }
+                });
+            };
+
+            e.Add(a);
+
+            ct.Register(() =>
+            {
+                e.Remove(a);
+                tcs.TrySetCanceled();
+            });
+
+            return tcs.Task;
+        }
+    }
+}
---
+++ Event.Shared/HandlerList.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+
+namespace System
+{
+    /// <summary>
+    /// Rx でいう Subject : IObserver, IObservable。
+    /// <see cref="IEvent{TArg}"/> の実装。
+    /// <see cref="Handler{TArg}"/> を束ねるクラス。
+    /// </summary>
+    /// <example>
+    /// <![CDATA[
+    /// public IEvent<T> SomeEvent { get { return _someEvent; } }
+    /// private HandlerList<T> _someEvent = new HandlerList<T>();
+    /// ]]>
+    /// </example>
+    /// <typeparam name="TArg">イベント引数の型。</typeparam>
+    public class HandlerList<TArg> : IEvent<TArg>
+    {
+        private Handler<TArg>[] _list;
+        private object _sync = new object();
+
+        public bool HasAny { get { return _list != null && _list.Length != 0; } }
+
+        public void Invoke(object sender, TArg args)
+        {
+            // ↓これだけでいいはずなんだけども、たぶん iOS AOT で動かない。
+            // var actions = System.Threading.Interlocked.Exchange(ref _list, null);
+            // _sync が要らなかったら、単なる配列の薄いラッパーだし、struct にしたいんだけども。
+            Handler<TArg>[] actions;
+            lock (_sync)
+            {
+                actions = _list;
+            }
+
+            if (actions == null) return;
+            foreach (var a in actions)
+            {
+                if (a != null) a(sender, args);
+            }
+        }
+
+        public void Add(Handler<TArg> action)
+        {
+            // こっちは CompareExchage
+            lock (_sync)
+            {
+                _list = _list == null
+                    ? new[] { action }
+                    : _list.Concat(new[] { action }).ToArray();
+            }
+        }
+
+        public void Remove(Handler<TArg> action)
+        {
+            lock (_sync)
+            {
+                _list = _list == null
+                    ? new Handler<TArg>[] { }
+                    : _list.Where(x => x != action).ToArray();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 非ジェネリック版。
+    /// </summary>
+    /// <remarks>
+    /// <see cref="Handler{TArg}"/> とは別型にしたいものの、実装がほぼコピペの似て非なるコードになるのがつらすぎて断念した結果。
+    /// void はジェネリックの敵。
+    /// </remarks>
+    public class HandlerList : HandlerList<Null>
+    {
+        public void Invoke(object sender) { Invoke(sender, null); }
+    }
+}
