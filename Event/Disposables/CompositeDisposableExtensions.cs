e1bfa093c13920d03ca2365b51868d35afc12bf7
niwanaga <iwanaga@orange-cube.jp>
2015-03-10 15:20:10 +0900

    CompositeDisposble.AddRange 拡張

---
+++ Event/Disposables/CompositeDisposableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace System.Disposables
+{
+    /// <summary>
+    /// <see cref="CompositeDisposable"/>に対する拡張メソッド。
+    /// </summary>
+    public static class CompositeDisposableExtensions
+    {
+        /// <summary>
+        /// まとめて<see cref="IDisposable"/>を追加。
+        /// </summary>
+        /// <param name="composite"></param>
+        /// <param name="items"></param>
+        public static void AddRange(this CompositeDisposable composite, IEnumerable<IDisposable> items)
+        {
+            foreach (var i in items)
+            {
+                composite.Add(i);
+            }
+        }
+    }
+}
