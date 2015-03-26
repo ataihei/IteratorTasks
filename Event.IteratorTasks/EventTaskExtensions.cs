43d3584f7d508c2003bb704a12366e5cce87e2ad
niwanaga <iwanaga@orange-cube.jp>
2015-03-26 10:48:19 +0900

    IEvent.ObserveOn 追加
    イベントが UI スレッドの外で起こる場合、イベントハンドラーはUIスレッドに戻さないとまずいことがある。それをやるためのメソッド。Rxもどき。

---
+++ Event.IteratorTasks/EventTaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+
+namespace System.Events
+{
+    /// <summary>
+    /// <see cref="IEvent{TArg}"/> と <see cref="IteratorTasks.Task"/> つなぎこみ関連拡張メソッド。
+    /// </summary>
+    public static class EventTaskExtensions
+    {
+        public static IEvent<T> ObserveOn<T>(this IEvent<T> e, IteratorTasks.TaskScheduler scheduler)
+        {
+            return Event.Create<T>(handler =>
+                e.Subscribe((sender, x) =>
+                    scheduler.Post(() =>
+                        handler.Invoke(sender, x)
+                    )
+                )
+            );
+        }
+    }
+}
