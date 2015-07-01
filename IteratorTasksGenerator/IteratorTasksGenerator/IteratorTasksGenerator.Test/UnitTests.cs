43872e56902841a882517a560a16dbbb2641d00b
赤坂玲音 <leon@orange-cube.jp>
2015-07-01 11:12:07 +0900

    テスト スクリプトが参照する mscorlib と System.Core を v3.5 に強制。

--- IteratorTasksGenerator/IteratorTasksGenerator/IteratorTasksGenerator.Test/UnitTests.cs
+++ IteratorTasksGenerator/IteratorTasksGenerator/IteratorTasksGenerator.Test/UnitTests.cs
@@ -4,13 +4,33 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using TestHelper;
-using IteratorTasksGenerator;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.CodeAnalysis.CSharp;
+using IteratorTasks;
 
 namespace IteratorTasksGenerator.Test
 {
     [TestClass]
     public class UnitTest : ConventionCodeFixVerifier
     {
+        protected override IEnumerable<MetadataReference> References
+        {
+            get
+            {
+                var x86 = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+                var programFilesPath = Directory.Exists(x86) ? x86 : Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+
+                var asmRoot = Path.Combine(programFilesPath, "Reference Assemblies", "Microsoft", "Framework", ".NETFramework", "v3.5", "Profile", "Client");
+                yield return MetadataReference.CreateFromFile(Path.Combine(asmRoot, "mscorlib.dll"));       //typeof(object).Assembly
+                yield return MetadataReference.CreateFromFile(Path.Combine(asmRoot, "System.Core.dll"));    //typeof(Enumerable).Assembly
+                yield return MetadataReference.CreateFromAssembly(typeof(CSharpCompilation).Assembly);
+                yield return MetadataReference.CreateFromAssembly(typeof(Compilation).Assembly);
+                yield return MetadataReference.CreateFromAssembly(typeof(Task).Assembly);
+                yield return MetadataReference.CreateFromAssembly(typeof(TaskEx).Assembly);
+            }
+        }
+
         [TestMethod]
         public void SimpleTask() => VerifyCSharpByConvention();
 
